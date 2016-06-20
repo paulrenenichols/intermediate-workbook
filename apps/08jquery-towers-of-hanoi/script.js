55ea26ac5985cd2ebb66e68bc3ee9482d354fce8
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2016-06-19 19:51:52 -0500

    paul-work-0005
    * jquery towers of hanoi

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,29 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+  var block = null;
+
+  $('[data-stack]').click(function() {
+    var $children = $(this).children();
+    if (block !== null) {
+      if (($children.length === 0) || (block.data('block') < $children.last().data('block'))) {
+        $(this).append(block);
+        block = null;
+      }
+    }
+    else if ($children.length > 0) {
+      block = $children.last();
+      block.detach();
+    }
+
+    checkForWin();
+  });
+
+  function checkForWin() {
+    $('[data-stack]').each(function () {
+      if (($(this).data('stack') > 1) && ($(this).children().length === 4)) {
+        $('#announce-game-won').text('You Won!');
+      }
+    });
+  }
+});
